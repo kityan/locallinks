1fdd62ef29c86cc93a35874158fc311611a9ba65
Pavel Kityan <pavel@kityan.ru>
2014-02-02 19:48:05 +0400

    Minor code refactoring, Fixes #1

--- firefox/mf.locallinks@kityan.ru/content/browserOverlay.js
+++ firefox/mf.locallinks@kityan.ru/content/browserOverlay.js
@@ -1,23 +1,31 @@
 function onLoad()
 {
-
 	gBrowser.removeEventListener("load", onLoad, false); //remove listener, no longer needed
-	$(this.contentWindow.document).find('body').delegate('span.myLocallink', "click",	function(e) 
-		{
+
+
+	var allowedHosts = ['', 'localhost'];
+	var selector =  'a.myLocallink';
+	var path2Explorer = "C:\\windows\\explorer.exe";
+
+	if (allowedHosts.indexOf(this.contentWindow.location.host) > -1)
+	{
+		$(this.contentWindow.document).find('body').delegate(selector, "click",	function(e) 
+			{
 		
-			// проверку хоста бы сделать!
-			e.preventDefault(); 
-			var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsIFile);
-			file.initWithPath("C:\\windows\\explorer.exe");
-			var process = Components.classes["@mozilla.org/process/util;1"].createInstance(Components.interfaces.nsIProcess);
-			process.init(file);
-			var converter = Components.classes['@mozilla.org/intl/scriptableunicodeconverter'].createInstance(Components.interfaces.nsIScriptableUnicodeConverter);
-			converter.charset = "windows-1251";
-			var path = [converter.ConvertFromUnicode(jQuery(this).data('path'))];
-			var args = [path];
-			process.run(false, args, args.length);
-		}
-	);
+				e.preventDefault(); 
+				var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsIFile);
+				file.initWithPath(path2Explorer);
+				var process = Components.classes["@mozilla.org/process/util;1"].createInstance(Components.interfaces.nsIProcess);
+				process.init(file);
+				var converter = Components.classes['@mozilla.org/intl/scriptableunicodeconverter'].createInstance(Components.interfaces.nsIScriptableUnicodeConverter);
+				converter.charset = "windows-1251";
+				var path = [converter.ConvertFromUnicode(jQuery(this).data('path'))];
+				var args = [path];
+				process.run(false, args, args.length);
+			}
+
+		);
+	}
 }
 
 gBrowser.addEventListener("load", onLoad, true);
